cf497eb63eeb13e85944f4417fac898337fbaa67
Marco Bruno <marco.bruno.br@gmail.com>
2015-05-01 17:14:32 -0300

    funcionando MAS com o codigo meio bosta

--- js/Elemento.js
+++ js/Elemento.js
@@ -1,23 +1,23 @@
-function Elemento(x, y, tamanho) {
+function Elemento(x, y, tamanho , coluna , indice) {
 	var x = x;
 	var y = y;
 	var tamanho = tamanho;
+	var coluna = coluna;
+	var indice = indice;
 
-	var calculaGravidade = function ($canvas,forcaDaGravidade,elementos) {
-		for(var i = 0; i < elementos.length ; i++) {
-			var el = elementos[i];
-		}
+	var calculaGravidade = function ($canvas,forcaDaGravidade,colunas) {
+		
+		if(y < $canvas.height - (tamanho * colunas[coluna]) - 1) {
+			y += forcaDaGravidade;
+		}else {
+			y = $canvas.height - tamanho * indice - 1;
+		};
 
-		if (y < $canvas.height-tamanho) {
-			y+=5;
-		} else {
-			y = $canvas.height-tamanho;
-		}	
 	};
 
-	this.desenha = function ($canvas,forcaDaGravidade,elementos) {
+	this.desenha = function ($canvas,forcaDaGravidade,colunas) {
 
-		calculaGravidade($canvas,forcaDaGravidade,elementos);
+		calculaGravidade($canvas,forcaDaGravidade,colunas);
 		var context = $canvas.getContext('2d');
 		
 		context.rect(x, y, tamanho, tamanho);	
--- js/game.js
+++ js/game.js
@@ -3,13 +3,14 @@
 	var context = $canvas.getContext('2d');
 	var gravidade = 10;
 	var elementos = [];
+	var colunas = [0,0,0,0,0];
 
 	function gameLoop() {
 		context.beginPath();
 		context.clearRect(0, 0, $canvas.width, $canvas.height);
 
 		elementos.forEach(function (el) {
-			el.desenha($canvas,gravidade,elementos);
+			el.desenha($canvas,gravidade,colunas);
 		});
 
 		win.requestAnimationFrame(gameLoop);
@@ -25,8 +26,9 @@
 		teclas[51] = 2; //Tecla 3
 		teclas[52] = 3; //Tecla 4
 		teclas[53] = 4; //Tecla 5
-		var novoElemento = new Elemento(teclas[ev.keyCode] * tamanho, (tamanho +20) * -1, tamanho);
+		colunas[teclas[ev.keyCode]]++;
 
+		var novoElemento = new Elemento(teclas[ev.keyCode] * tamanho, (tamanho +20) * -1, tamanho , teclas[ev.keyCode],colunas[teclas[ev.keyCode]]);
 		console.log(teclas[ev.keyCode]);
 
 		elementos.push(novoElemento);
